63797a25276eed00f054a96f132b61517ef07653
Subhas <goalkeepersubhas07@gmail.com>
2025-11-07 21:23:30 +0530

    Added Client

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center font-bold text-2xl underline'>
+      This is the APP
+    </div>
+  )
+}
+
+export default App
