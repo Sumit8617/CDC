7839a279c9d4c08ae28b423f83f8376214c5f89c
sumitprasad02012004@gmail.com <sumitprasad02012004@gmail.com>
2025-12-15 01:31:16 +0530

    Remove the cookieconfig file

--- Server/config/Cookie.config.js
+++
@@ -1,9 +0,0 @@
-const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production" ? true : false,
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  path: "/",
-  maxAge: 1000 * 60 * 60 * 24,
-};
-
-export { cookieOptions };
