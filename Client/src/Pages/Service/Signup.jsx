5d19fd72d1a8043eff8ecb449c61ed144ea68469
Subhas <goalkeepersubhas07@gmail.com>
2026-03-28 00:34:31 +0530

    update the otp functionality

--- Client/src/Pages/Service/Signup.jsx
+++ Client/src/Pages/Service/Signup.jsx
@@ -40,7 +40,7 @@ const Signup = () => {
 
   const password = watch("password");
 
-  /*   const onSendOtp = async () => {
+  const onSendOtp = async () => {
     const { email, fullName } = methods.getValues();
     if (!email || !fullName) {
       return alert("Please enter Full Name and Email first");
@@ -52,10 +52,10 @@ const Signup = () => {
     const otp = methods.getValues("otp");
     if (!otp) return alert("Enter OTP first");
     await handleVerifyOtp(otp);
-  }; */
+  };
 
   const onSubmit = async (data) => {
-    // if (!otpVerified) return alert("Verify OTP first");
+    if (!otpVerified) return alert("Verify OTP first");
     await handleSignup(data);
     reset();
     navigate("/");
@@ -115,7 +115,7 @@ const Signup = () => {
                     rules={{ required: "Email is required" }}
                   />
 
-                  {/* {!otpSent && (
+                  {!otpSent && (
                     <div className="flex items-center mt-3">
                       <Button
                         type="button"
@@ -128,7 +128,7 @@ const Signup = () => {
                         Send OTP
                       </Button>
                     </div>
-                  )} */}
+                  )}
                 </div>
                 {/* OTP Verification Section */}
                 {otpSent && (
@@ -147,7 +147,7 @@ const Signup = () => {
                         variant="outline"
                         size="sm"
                         round="md"
-                        // onClick={onVerifyOtp}
+                        onClick={onVerifyOtp}
                         disabled={otpVerified}
                         className="h-[38px] whitespace-nowrap"
                       >
